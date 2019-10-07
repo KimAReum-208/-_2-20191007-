2eef4052da63a733e8a7c9a85f40fb0fd3a704ac
areum <rladkfma2030@gmail.com>
2019-10-07 13:37:31 +0900

    HashMap 생성과 한 라인씩 읽어들이는 메소드를 작성하였습니다.

--- FileReaderTest.java
+++ FileReaderTest.java
@@ -1,5 +1,5 @@
 import java.io.*;
-
+import java.util.*;
 /**
  * 여기에 FileReader 클래스 설명을 작성하십시오.
  * 
@@ -8,9 +8,24 @@ import java.io.*;
  */
 public class FileReaderTest{
     public static void main (String args[]){
+        HashMap<String, Integer> score = new HashMap<String, Integer>(); 
+
         try{
             FileReader fr = new FileReader("c:\\Temp\\inputData20191007.txt");
-            
+            Scanner scanner = new Scanner(fr);
+
+            String key;
+            String value;
+            while(scanner.hasNext()){
+                String word = scanner.nextLine();
+                String[] s = word.split(" ");
+
+                key = s[0];
+                value = s[1];
+                
+                System.out.println(key + value);
+                
+            }
         }catch(IOException e){
             System.out.println("파일을 찾을 수 없습니다");
         }
